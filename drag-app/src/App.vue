a8e934b95e453de85b340d7ac4daf93008289a9d
abusizhishen <abusizhishen@gmail.com>
2021-12-23 00:41:35 +0800

    init

---
+++ drag-app/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <draggable v-model="list">
+      <transition-group tag="div" class="grid" name="grid">
+        <div class="cell" v-for="item in list" :key="item">{{item}}</div>
+      </transition-group>
+    </draggable>
+  </div>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  name: "App",
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      list: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 100px);
+  grid-template-rows: repeat(3, 100px);
+  grid-gap: 0.2em;
+}
+
+.grid-move {
+  transition: all 0.3s;
+}
+
+.cell {
+  background-color: lightblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
